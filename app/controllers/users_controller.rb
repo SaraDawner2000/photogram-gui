4b40522d594e76174ba6e0ef21e86c233bd02a1e
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-17 14:50:47 -0500

    add update & delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,18 +11,34 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.username = params[:query_username]
+
     if @user.valid?
       @user.save
       redirect_to "/users", notice: "User created successfully"
     else
-      redirect_to "/users", notice: "User failed to create successfully"
+      redirect_to "/users", alert: "User failed to create successfully"
     end
   end
 
   def update
+    id = params[:path_id]
+    @user = User.where(id:).first
+    @user.username = params[:query_username]
+
+    if @user.valid?
+      @user.save
+      redirect_to "/users/#{@user.id}", notice: "User updated successfully"
+    else
+      redirect_to "/users/#{@user.id}", alert: "User failed to update successfully"
+    end
   end
 
   def destroy
+    id = params[:path_id]
+    @user = User.where(id:).first
+    @user.destroy
+
+    redirect_to "/users", notice: "User deleted successfully"
   end
 
 end
