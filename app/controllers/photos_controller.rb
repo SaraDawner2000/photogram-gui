5451a486827152349a5fa4a31abe147231ef3bf1
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-17 15:14:59 -0500

    add update & delete photo functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,13 +20,30 @@ class PhotosController < ApplicationController
     else
       redirect_to "/photos", alert: "Photo failed to post successfully"
     end
-
   end
 
   def update
+    id = params[:path_id]
+    @photo = Photo.where(id:).first
+
+    @photo.image = params[:query_image_url]
+    @photo.caption = params[:query_caption]
+    
+    if @photo.valid?
+      @photo.save
+      redirect_to "/photos/#{@photo.id}", notice: "Photo updated successfully"
+    else
+      redirect_to "/photos/#{@photo.id}", alert: "Photo failed to update successfully"
+    end
   end
 
   def destroy
+    id = params[:path_id]
+    @photo = Photo.where(id:).first
+
+    @photo.destroy
+
+    redirect_to "/photos", notice: "Photo deleted successfully"
   end
 
 end
